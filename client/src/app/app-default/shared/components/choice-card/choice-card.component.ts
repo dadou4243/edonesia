8080f62cd297076efdddda0714470dde90207d3b
Lasry David <lasry.david@gmail.com>
2019-10-16 13:25:13 +0300

    add ChoiceCard component

---
+++ client/src/app/app-default/shared/components/choice-card/choice-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choice-card',
+  templateUrl: './choice-card.component.html',
+  styleUrls: ['./choice-card.component.scss']
+})
+export class ChoiceCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- client/src/app/app-default/shared/shared.module.ts
+++ client/src/app/app-default/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BookTripComponent } from './book-trip/book-trip.component';
 import { StripePaymentComponent } from './stripe-payment/stripe-payment.component';
+import { ChoiceCardComponent } from './components/choice-card/choice-card.component';
 
 @NgModule({
-  declarations: [BookTripComponent, StripePaymentComponent],
+  declarations: [BookTripComponent, StripePaymentComponent, ChoiceCardComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [
     CommonModule,
