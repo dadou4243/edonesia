65fff6b1f412a4f8ded122f2be2590a2df8bac6a
Lasry David <lasry.david@gmail.com>
2019-10-17 01:50:05 +0300

    fix dates trip input on keyup and validation

--- client/src/app/app-default/trip-request/components/dates-trip/dates-trip.component.ts
+++ client/src/app/app-default/trip-request/components/dates-trip/dates-trip.component.ts
@@ -20,6 +20,7 @@ export class DatesTripComponent implements OnInit, OnChanges {
 
   tempDepartureDate;
   tempArrivalDate;
+  error: string;
 
   constructor() {}
 
@@ -40,10 +41,35 @@ export class DatesTripComponent implements OnInit, OnChanges {
     this.tempArrivalDate = this.arrivalDate;
   }
 
-  onClickConfirm() {
+  onDepartureDateChange(date) {
+    this.tempDepartureDate = date;
     this.dateConfirmed.emit({
       arrivalDate: this.tempArrivalDate,
       departureDate: this.tempDepartureDate
     });
+  }
+
+  onArrivaleDateChange(date) {
+    this.tempArrivalDate = date;
+    this.dateConfirmed.emit({
+      arrivalDate: this.tempArrivalDate,
+      departureDate: this.tempDepartureDate
+    });
+  }
+
+  onClickConfirm() {
+    this.error = '';
+
+    console.log('this.tempDepartureDate:', this.tempDepartureDate);
+    console.log('this.tempArrivalDate:', this.tempArrivalDate);
+    if (!this.tempDepartureDate || !this.tempArrivalDate) {
+      this.error = 'You must fill a departure and arrival date.';
+      return;
+    }
+
+    // this.dateConfirmed.emit({
+    //   arrivalDate: this.tempArrivalDate,
+    //   departureDate: this.tempDepartureDate
+    // });
   }
 }
