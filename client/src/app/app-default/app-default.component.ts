70319e53f91db4d1992b9ae6d6d37eebb0b5d22f
Lasry David <lasry.david@gmail.com>
2019-11-03 06:26:19 +0200

    conditional header

--- client/src/app/app-default/app-default.component.ts
+++ client/src/app/app-default/app-default.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Logout } from '../store/user';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-default',
   templateUrl: './app-default.component.html',
   styleUrls: ['./app-default.component.scss']
 })
-export class AppDefaultComponent {
-  constructor(private store: Store<any>) {}
+export class AppDefaultComponent implements OnInit {
+  showHeader: boolean;
+
+  constructor(private store: Store<any>, private router: Router) {}
+
+  ngOnInit() {
+    this.showHeader = this.router.url === '/request' ? false : true;
+
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe((location: NavigationEnd) => {
+        this.showHeader = location.url === '/request' ? false : true;
+      });
+  }
 
   onClickLogout() {
     // console.log('LOGOUT APP DEFAULT');
