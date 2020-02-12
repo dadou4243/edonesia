2203c7ac0ca9d6001efbdba4ff908ce7448a51b8
Lasry David <lasry.david@gmail.com>
2020-02-12 23:40:39 +0200

    fix phone prefix

--- client/src/app/app-default/trip-request/components/user-form/user-form.component.ts
+++ client/src/app/app-default/trip-request/components/user-form/user-form.component.ts
@@ -1,33 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PhoneNumberComponent } from 'ngx-international-phone-number';
 
 @Component({
-  selector: 'app-user-form',
-  templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.scss']
+    selector: 'app-user-form',
+    templateUrl: './user-form.component.html',
+    styleUrls: ['./user-form.component.scss']
 })
-export class UserFormComponent implements OnInit {
-  userForm;
-  model = {
-    phone_number: '34234443'
-  };
+export class UserFormComponent implements OnInit, AfterViewInit {
+    userForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.userForm = this.formBuilder.group({
-      firstName: ['', [Validators.required, Validators.minLength(10)]],
-      lastName: ['', [Validators.required, Validators.minLength(10)]],
-      email: ['', [Validators.email]],
-      phonePrefix: ['+1', [Validators.required, Validators.minLength(10)]],
-      phoneNumber: ['', [Validators.required, Validators.minLength(10)]],
-      country: ['', [Validators.required]],
-      contact: ['', [Validators.required]],
-      confirmContact: ['', [Validators.required]],
-      generalTerms: ['', [Validators.required]],
-    });
-  }
+    @ViewChild('phonePrefixElement', {static: false, read: ElementRef}) phonePrefixElement: ElementRef;
 
-  ngOnInit() {
-    // this.userForm.controls.phonePrefix.disable();
-  }
+    constructor(private formBuilder: FormBuilder,
+                private renderer: Renderer2) {
+        this.userForm = this.formBuilder.group({
+            firstName: ['', [Validators.required, Validators.minLength(10)]],
+            lastName: ['', [Validators.required, Validators.minLength(10)]],
+            email: ['', [Validators.email]],
+            phonePrefix: ['+1', [Validators.required]],
+            phoneNumber: ['', [Validators.required, Validators.minLength(10)]],
+            country: ['', [Validators.required]],
+            contact: ['', [Validators.required]],
+            confirmContact: ['', [Validators.required]],
+            generalTerms: ['', [Validators.required]],
+        });
+    }
+
+    ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        console.log('childComponent', this.phonePrefixElement);
+        const phonePrefixInputElement = this.phonePrefixElement.nativeElement.children[0].children[1];
+        console.log(phonePrefixInputElement);
+        this.renderer.setAttribute(phonePrefixInputElement, 'disabled', '');
+        this.renderer.addClass(phonePrefixInputElement, 'prefix-input');
+
+    }
 
 }
